4c548666fa7e80588483385a243d05736a036333
Renato Crudo <re.crudo.85@gmail.com>
2019-05-27 11:27:59 -0300

    Adicionando array para guardar os todos

---
+++ todos.js
@@ -0,0 +1,10 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+//criando array para armazenar os todos
+var todos = [
+  "Fazer café",
+  "Estudar Javascript",
+  "Acessar comunidade da Rocketseat"
+];
