e2fdc7a18909fa22c12f6cccd3faa8aadb27315b
Renato Crudo <re.crudo.85@gmail.com>
2019-05-27 14:00:12 -0300

    Adicionando link de excluir

--- todos.js
+++ todos.js
@@ -16,7 +16,14 @@ function renderTodos() {
     var todoElement = document.createElement("li");
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+    var linkText = document.createTextNode("Excluir");
+
+    linkElement.appendChild(linkText);
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
     listElement.appendChild(todoElement);
   }
 }
